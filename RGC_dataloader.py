0798143d9d632841094112d367d6759f04c29879
scapocyan <slcapocyan@gmail.com>
2023-08-04 17:30:27 -0400

    first commit

---
+++ RGC_dataloader.py
@@ -0,0 +1,133 @@
+import os
+import cv2
+from PIL import Image
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+from scipy import stats as st
+
+class RGC_Dataset(Dataset):
+    def __init__(self, image_dir, label_dir, transform=None):
+        self.image_dir = image_dir
+        self.label_dir = label_dir
+        self.transform = transform
+        self.images = os.listdir(image_dir)
+        self.labels = os.listdir(label_dir)
+    
+    def __len__(self):
+        return len(self.images)
+    
+    def __getitem__(self, index):
+        """This function should return an image and its label given an index. The images and labels are 16-bit grayscale images.
+        The images and labels are both of size 1040x1392. The images are stored in the image_dir and the labels are stored in the label_dir.
+
+        Args:
+            index (int): The index of the image and label to be returned.
+        """
+        img_path = os.path.join(self.image_dir, self.images[index])
+        label_path = os.path.join(self.label_dir, self.labels[index])
+        image = Image.open(img_path)
+        label = Image.open(label_path)
+        
+        if self.transform is not None:
+            image = self.transform(image)
+            label = self.transform(label)
+
+        # Convert to numpy array before resizing
+        image = np.array(image)
+        label = np.array(label)
+
+        # Resize the images and labels to 256x256
+        desired_size = (256, 256)
+        image = cv2.resize(image, desired_size, interpolation=cv2.INTER_LANCZOS4)
+        label = cv2.resize(label, desired_size, interpolation=cv2.INTER_LANCZOS4)
+
+        # Image Normalization
+        image = image.astype(np.double) # convert to float before normalization
+        image_mode = st.mode(image, axis=None)
+        image -= image_mode[0]
+        image[image < 0] = 0
+        image /= np.max(image)
+        image = torch.from_numpy(image)
+
+        # Label normalization
+        label = label.astype(np.double)
+        label_mode = st.mode(label, axis=None)
+        label -= label_mode[0]
+        label[label < 0] = 0
+        label /= np.max(label)
+        label = torch.from_numpy(label)
+
+        # # Normalize the images and labels by subtracting the mode pixel value from each pixel and dividing by the max pixel value
+        # image_elements, image_counts = torch.unique(image.view(-1), return_counts=True)
+        # image_mode_index = torch.argmax(image_counts)
+        # image_mode = image_elements[image_mode_index]
+        # image = image - image_mode
+        # image[image < 0] = 0
+        # image = image / torch.max(image)
+
+        # label_elements, label_counts = torch.unique(label.view(-1), return_counts=True)
+        # label_mode_index = torch.argmax(label_counts)
+        # label_mode = label_elements[label_mode_index]
+        # label = label - label_mode
+        # label[label < 0] = 0
+        # label = label / torch.max(label)
+
+        # # Resize the images and labels to 256x256
+        # image = transforms.Resize((256, 256))(image)
+        # label = transforms.Resize((256, 256))(label)
+
+        return image, label
+
+# Load the dataset
+image_dir = r'C:\Users\Sam\Documents\Python Scripts\RGC\RGC_unadjusted_dataset\train_images'
+label_dir = r'C:\Users\Sam\Documents\Python Scripts\RGC\RGC_unadjusted_dataset\train_labels'
+training_images = RGC_Dataset(image_dir, label_dir, transform=None)
+
+torch.manual_seed(0)
+
+# Split the dataset into training and validation sets
+train_size = int(0.9 * len(training_images))
+val_size = len(training_images) - train_size
+train_dataset, val_dataset = torch.utils.data.random_split(training_images, [train_size, val_size])
+
+train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=True)
+
+# # Print the shape of the inputs in the dataloaders
+# for image, label in train_dataloader:
+#     image.unsqueeze_(1)
+#     label.unsqueeze_(1)
+#     print(image.shape)
+#     print(label.shape)
+#     break
+
+# # Check the data type of the images and labels
+# for image, label in train_dataloader:
+#     print(image.dtype)
+#     print(label.dtype)
+#     break
+
+# # # Check the shape of the images and labels
+# # counter = 0
+# # for image, label in train_dataloader:
+# #     print(image.shape)
+# #     print(label.shape)
+# #     if counter == 2:
+# #         break
+# #     counter += 1
+
+# # # Display a random sample image and label
+# # image, label = train_dataset[56]
+# # plt.figure(1)
+# # plt.imshow(image, cmap='gray')
+# # plt.figure(2)
+# # plt.imshow(label, cmap='gray')
+# # plt.show()
+
+# # Print the size of the training and validation sets
+# print(f"Training set size: {len(train_dataset)}")
+# print(f"Validation set size: {len(val_dataset)}")
+
---
+++ train.py
@@ -0,0 +1,184 @@
+import torch
+import matplotlib.pyplot as plt
+import torch.optim as optim
+from RGC_dataloader import RGC_Dataset
+from RGC_UNet_model import RGC_UNet
+from tqdm import tqdm
+
+# Define the directory paths
+IMAGE_DIR = r'C:\Users\Sam\Documents\Python Scripts\RGC\RGC_unadjusted_dataset\train_images'
+LABEL_DIR = r'C:\Users\Sam\Documents\Python Scripts\RGC\RGC_unadjusted_dataset\train_labels'
+
+# Define the batch sizes
+TRAIN_BATCH_SIZE = 32
+VALID_BATCH_SIZE = 8
+
+# Load the dataset
+training_set = RGC_Dataset(IMAGE_DIR, LABEL_DIR, transform=None)
+
+train_size = int(0.9 * len(training_set))
+val_size = len(training_set) - train_size
+
+# Set the random seed before splitting the dataset
+torch.manual_seed(0)
+
+train_dataset, val_dataset = torch.utils.data.random_split(training_set, [train_size, val_size])
+
+# Create the dataloaders
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=TRAIN_BATCH_SIZE, shuffle=True)
+val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=VALID_BATCH_SIZE, shuffle=True)
+
+
+# Define Negative Pearson Correlation Coefficient loss
+def NPCC_loss(outputs, labels):
+    # Calculate the mean of the outputs and labels
+    outputs_mean = torch.mean(outputs)
+    labels_mean = torch.mean(labels)
+
+    # Subract the mean from the outputs and labels
+    outputs = outputs - outputs_mean
+    labels = labels - labels_mean
+
+    # Multiply the outputs and labels
+    outputs_labels = outputs * labels
+
+    # Square the outputs and labels
+    outputs_squared = torch.square(outputs)
+    labels_squared = torch.square(labels)
+
+    # Calculate the sum of the elements in the outputs_labels, outputs_squared and labels_squared tensors
+    outputs_labels_sum = torch.sum(outputs_labels)
+    outputs_squared_sum = torch.sum(outputs_squared)
+    labels_squared_sum = torch.sum(labels_squared)
+
+    # Calculate the square root of outputs_squared_sum and labels_squared_sum
+    outputs_squared_sum_sqrt = torch.sqrt(outputs_squared_sum)
+    labels_squared_sum_sqrt = torch.sqrt(labels_squared_sum)
+
+    # Calculate the numerator and denominator
+    numerator = -1 * outputs_labels_sum
+    denominator = outputs_squared_sum_sqrt * labels_squared_sum_sqrt + 1e-10
+
+    # Calculate the NPCC
+    npcc_loss = torch.div(numerator, denominator)
+
+    return npcc_loss
+
+# Define the model and loss
+model = RGC_UNet()
+model = model.double()
+criterion = NPCC_loss
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
+
+
+# Evaluating data to get the train/test accuracy and train/test loss
+def evaluate(dataloader):
+    total = 0
+    correct = 0
+    loss_sum = 0
+
+    for images, labels in dataloader:
+        images = images.to(device)
+        labels = labels.to(device)
+
+        outputs = model(images)
+
+        total += labels.size(0)
+        correct += (outputs == labels).sum()
+        loss_sum += criterion(outputs, labels)
+
+    # Calculate the training and validation accuracies and losses
+    accuracy = 100 * correct / total
+    total_loss = loss_sum.cpu().item() / total
+
+    if dataloader == train_dataloader:
+        print(f"Train Accuracy: {accuracy:.2f}%, Train Loss: {total_loss:.5f}")
+    elif dataloader == val_dataloader:
+        print(f"Validation Accuracy: {accuracy:.2f}%, Validation Loss: {total_loss:.5f}")
+
+    return accuracy, total_loss
+
+
+# Set up the training loop
+def train(model,
+          optimizer,
+          loss_fn,
+          epochs=500,
+          train_dataloader=train_dataloader,
+          val_dataloader=val_dataloader,
+          **kwargs):
+    
+    train_accuracies = []
+    train_losses = []
+    val_accuracies = []
+    val_losses = []
+
+    for epoch in range(epochs):
+        print(f"Epoch {epoch + 1}/{epochs}\n-------------------------------")
+
+        loop = tqdm(train_dataloader)
+
+        for batch, (images, labels) in enumerate(loop):
+            images = images.to(device)
+            labels = labels.to(device)
+            images = images.unsqueeze(1)
+            labels = labels.unsqueeze(1)
+
+            # Forward pass
+            outputs = model(images)
+            loss = loss_fn(outputs, labels)
+
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        # Evaluate the model
+        train_accuracy, train_loss = evaluate(train_dataloader)
+        val_accuracy, val_loss = evaluate(val_dataloader)
+
+        # Append the train and validation accuracies and losses to the lists
+        train_accuracies.append(train_accuracy)
+        train_losses.append(train_loss)
+        val_accuracies.append(val_accuracy)
+        val_losses.append(val_loss)
+    
+    # Plot the train and validation accuracies and losses
+    plt.figure(1)
+    plt.plot(train_accuracies, label='Train')
+    plt.plot(val_accuracies, label='Validation')
+    plt.title('Train and Validation Accuracies')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.show()
+
+    plt.figure(2)
+    plt.plot(train_losses, label='Train')
+    plt.plot(val_losses, label='Validation')
+    plt.title('Train and Validation Losses')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+    raise NotImplementedError
+
+
+# Hyperparameters
+EPOCHS = 3
+
+# Set up the optimizer
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Train the model
+train(model, optimizer, criterion, EPOCHS, train_dataloader, val_dataloader)
+
