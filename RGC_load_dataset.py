c754e2a2c6c540fd9b6ae4336995de18966ef795
scapocyan <slcapocyan@gmail.com>
2023-11-01 17:02:39 -0400

    eval to .py, removed transform in load

--- RGC_load_dataset.py
+++ RGC_load_dataset.py
@@ -9,7 +9,6 @@ class RGC_Dataset(Dataset):
     def __init__(self, image_dir, label_dir):
         self.image_dir = image_dir
         self.label_dir = label_dir
-        self.transform = transform
         self.images = sorted(os.listdir(image_dir))
         self.labels = sorted(os.listdir(label_dir))
     
